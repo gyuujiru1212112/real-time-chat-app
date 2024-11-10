795e7b00fcde070deea9af882b39ef7b9a3aeab1
gyuujiru1212112 <ydjing121@gmail.com>
2024-11-09 19:13:02 -0500

    cli init

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,42 @@
-#[macro_use] extern crate rocket;
+use std::io::{self, Write};
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Welcome to the real time chat app!");
+    println!("Type 'help' to see available commands.");
+    // todo launch the server
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    // get the command
+    loop {
+        print!("> ");
+        io::stdout().flush()?;
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+        let input = input.trim();
+
+        match input {
+            "help" => {
+                // todo help message
+            }
+            command if input.starts_with("signup") => {
+
+            }
+            command if input.starts_with("login") => {
+
+            }
+            command if input.starts_with("logout") => {
+
+            }
+            
+            "q" => {
+                // exit the app
+                println!("App is shutting down...");
+                break;
+            }
+            _ => {
+                println!("Unknown command. Type 'help' to see available commands.")
+            }
+        }
+    }
+    Ok(())
 }
