6d6054a28cf91a7881401bb786828ede6b33e83c
gyuujiru1212112 <ydjing121@gmail.com>
2024-11-19 20:17:46 -0500

    move functions into a file and add validation fn

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,122 +1,13 @@
+mod commands;
+
 use std::io::{self, Write};
 use reqwest::Client;
-use rocket::serde::ser::StdError;
-use rocket::serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize)]
-struct SignupInfo {
-    username: String,
-    email: String,
-    password: String,
-}
-
-
-#[derive(Deserialize, Serialize)]
-struct UserLogin {
-    username: String,
-    password: String,
-}
-
-#[derive(Deserialize, Serialize)]
-struct UserLogout {
-    username: String,
-    session_id: String,
-}
-
-async fn signup(client: &Client, username: String, email: String, password: String) -> Result<(), Box<dyn StdError>>
-{
-    let url = "http://localhost:8000/chatapp/user/signup"; // signup endpoint
-
-    // Prepare the signup data
-    let signup_info = SignupInfo { username, email, password };
-
-    // Send the POST request
-    let response = client
-        .post(url)
-        .json(&signup_info)
-        .send()
-        .await?;
-
-    // Check if the response was successful
-    if response.status().is_success() {
-        println!("Signup successfully!");
-    } else {
-        let error_message = response.text().await?;
-        println!("Error: failed to signup user: {}", error_message);
-    }
-
-    Ok(())
-}
-
-async fn login(client: &Client, username: String, password: String) -> Result<(), Box<dyn StdError>>
-{
-    let url = "http://localhost:8000/chatapp/user/login";
-    let login_info = UserLogin { username, password };
-
-    // Send the POST request
-    let response = client
-        .post(url)
-        .json(&login_info)
-        .send()
-        .await?;
-
-    // Check if the response was successful
-    if response.status().is_success() {
-        // todo get the session id
-        println!("login successfully!");
-    } else {
-        let error_message = response.text().await?;
-        println!("Error: failed to login user: {}", error_message);
-    }
-
-    Ok(())
-}
-
-async fn logout(client: &Client) -> Result<(), Box<dyn StdError>>
-{
-    // todo session id
-    Ok(())
-}
-
-async fn check_user_status(client: &Client, user: String) -> Result<(), Box<dyn StdError>>
-{
-    Ok(())
-}
-
-async fn list_active_users(client: &Client) -> Result<(), Box<dyn StdError>>
-{
-    Ok(())
-}
-
-async fn create_private_chat(client: &Client, user: String) -> Result<(), Box<dyn StdError>>
-{
-    // todo enter a child CLI interface
-    Ok(())
-}
-
-async fn resume_private_chat(client: &Client, chatId: String) -> Result<(), Box<dyn StdError>>
-{
-    // todo enter a child CLI interface
-    Ok(())
-}
-
-async fn create_chat_room(client: &Client, room_name: String) -> Result<(), Box<dyn StdError>>
-{
-    // todo enter a child CLI interface
-    Ok(())
-}
-
-async fn resume_chat_room(client: &Client, chatId: String) -> Result<(), Box<dyn StdError>>
-{
-    // todo enter a child CLI interface
-    Ok(())
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Welcome to the real time chat app!");
     println!("Type 'help' to see available commands.");
-    // todo launch the server
+    // client
     let client = Client::new();
 
     // get the command
@@ -146,7 +37,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 let username = "yiduo";
                 let email = "ydjing121@gmail.com";
                 let password = "!Abcd";
-                signup(&client, username.to_string(), email.to_string(), password.to_string()).await?;
+                commands::signup(&client, username.to_string(), email.to_string(), password.to_string()).await?;
             }
             command if input.starts_with("login") => {
 
