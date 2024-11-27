1e27b2e38e9687fcf78819aa5188d920b327c6f3
Kayleigh McNeil <kaycarrollmcneil@gmail.com>
2024-11-26 23:25:29 -0500

    added endpoint for getting list of active users or the status of specific user

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,9 +2,10 @@ mod database;
 
 use database::DbManager;
 use rocket::http::Status;
+use rocket::request::{FromRequest, Outcome, Request};
 use rocket::serde::json::Json;
 use rocket::serde::{Deserialize, Serialize};
-use rocket::{launch, post, routes};
+use rocket::{get, launch, post, routes};
 use uuid::Uuid;
 
 #[derive(Deserialize, Serialize)]
@@ -21,10 +22,16 @@ struct UserLogin {
 }
 
 #[derive(Deserialize, Serialize)]
-struct UserLogout {
+struct UserReqInfo {
     username: String,
     session_id: String,
 }
+
+// #[derive(Deserialize, Serialize)]
+// struct UserStatus {
+//     username: String,
+//     session_id: String,
+// }
 
 #[post("/signup", format = "json", data = "<signup_info>")]
 async fn signup<'a>(
@@ -62,7 +69,7 @@ async fn login(
             } else {
                 let session_id: String = Uuid::new_v4().to_string();
                 let success = db_manager
-                    .set_user_session_id(&user_login.username, &session_id)
+                    .set_user_session_id(&user_login.username, Some(&session_id))
                     .await;
                 if success {
                     let response_body = format!(
@@ -85,28 +92,121 @@ async fn login(
     }
 }
 
-#[post("/logout", format = "json", data = "<user_logout>")]
-async fn logout(user_logout: Json<UserLogout>, db_manager: &rocket::State<DbManager>) -> Status {
-    match db_manager.get_user(&user_logout.username).await {
+#[post("/logout", format = "json", data = "<user>")]
+async fn logout(user: Json<UserReqInfo>, db_manager: &rocket::State<DbManager>) -> Status {
+    let valid_session: bool =
+        is_session_id_valid(&user.username, &user.session_id, db_manager.inner()).await;
+
+    if valid_session {
+        let success = db_manager.set_user_session_id(&user.username, None).await;
+        if success {
+            Status::Ok
+        } else {
+            Status::InternalServerError
+        }
+    } else {
+        Status::Unauthorized
+    }
+}
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for UserReqInfo {
+    type Error = ();
+    async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        let session_id = request.headers().get_one("session_id").unwrap_or("");
+        let username = request.headers().get_one("username").unwrap_or("");
+
+        Outcome::Success(UserReqInfo {
+            username: String::from(username),
+            session_id: String::from(session_id),
+        })
+    }
+}
+
+#[get("/status?<username..>")]
+async fn active_users(
+    username: Option<String>,
+    user_info: UserReqInfo,
+    db_manager: &rocket::State<DbManager>,
+) -> (Status, String) {
+    let valid_session: bool = is_session_id_valid(
+        &user_info.username,
+        &user_info.session_id,
+        db_manager.inner(),
+    )
+    .await;
+
+    if valid_session {
+        match username {
+            Some(name) => match db_manager.get_user(&name).await {
+                Some(user) => {
+                    let status = match user.session_id {
+                        Some(_) => "ACTIVE",
+                        None => "INACTIVE",
+                    };
+                    (
+                        Status::Ok,
+                        format!(
+                            "[{{\"username\": \"{}\", \"status\": \"{}\"}}]",
+                            user.username, status
+                        ),
+                    )
+                }
+                None => (
+                    Status::NotFound,
+                    format!(
+                        "[{{\"username\": \"{}\", \"status\": \"NOT_FOUND\"}}]",
+                        name
+                    ),
+                ),
+            },
+            None => match db_manager.get_active_users().await {
+                Some(users) => {
+                    let mut ret_message = String::new();
+                    ret_message += "[";
+                    for user in users.iter() {
+                        ret_message += &format!(
+                            "{{\"username\": \"{}\", \"status\": \"ACTIVE\"}},",
+                            user.username
+                        );
+                    }
+                    ret_message.pop(); // Remove last character "," from the string.
+                    ret_message += "]";
+
+                    (Status::Ok, ret_message)
+                }
+                None => (Status::Ok, String::from("[]")),
+            },
+        }
+    } else {
+        (Status::Unauthorized, String::from(""))
+    }
+}
+
+async fn is_session_id_valid(
+    username: &String,
+    given_session_id: &String,
+    db_manager: &DbManager,
+) -> bool {
+    let mut is_valid: bool = false;
+    match db_manager.get_user(username).await {
         Some(user) => match user.session_id {
             Some(session_id) => {
-                if session_id != user_logout.session_id {
-                    Status::Unauthorized
+                if session_id == *given_session_id {
+                    println!("Valid username and session_id pair.");
+                    is_valid = true;
                 } else {
-                    let success = db_manager
-                        .set_user_session_id(&user_logout.username, &String::new())
-                        .await;
-                    if success {
-                        Status::Ok
-                    } else {
-                        Status::InternalServerError
-                    }
+                    println!(
+                        "Given session_id does not match with expected value for user \'{}\'.",
+                        username
+                    );
                 }
             }
-            None => Status::Unauthorized,
+            None => println!("No current session_id for user \'{}\'.", username),
         },
-        None => Status::Unauthorized,
+        None => println!("Invalid username. Could not find user \'{}\'.", username),
     }
+    is_valid
 }
 
 #[launch]
@@ -115,5 +215,8 @@ async fn rocket() -> _ {
     let db_url: String = String::from("mysql://chatserver:ServerPass123@localhost:3306/chatapp");
     rocket::build()
         .manage::<DbManager>(DbManager::new(db_url).await.unwrap())
-        .mount("/chatapp/user/", routes![signup, login, logout])
+        .mount(
+            "/chatapp/user/",
+            routes![signup, login, logout, active_users],
+        )
 }
